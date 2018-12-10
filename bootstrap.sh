a7473c809fc3a8af38d2cc5197ee8ebf7f4745a2
loujiayu <loujiayu7@gmail.com>
2018-12-10 14:43:21 +0800

    add Vundle.vim

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ ln -sf $PWD/.functions ~/.functions
 ln -sf $PWD/.aliases ~/.aliases
 ln -sf $PWD/.zshrc ~/.zshrc
 ln -sf $PWD/.vimrc ~/.vimrc
-ln -sf $PWD/.vim ~/.vim
+# ln -sf $PWD/.gitconfig ~/.gitconfig
+ln -sf $PWD/.vim ~
 
 zsh
